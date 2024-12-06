76b9f11b3bdaa1faef222f3e80218ff475b6c558
chaitanyakumar99 <gchkumar999@gmail.com>
2024-12-06 22:01:36 +0530

    txt

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Functionalform from './Createform/Functionalform'
-const App=()=>{
+import Password from './Vlidate password/Password'
+const App = () => {
     return <div>
         <h1>App Component</h1>
-        <Functionalform/>
+        <Password />
     </div>
 }
 
